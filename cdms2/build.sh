9ceeffaf0da82147b4205d5d571e6a122b804660
Charles Doutriaux <doutriaux1@llnl.gov>
2016-03-17 15:47:05 -0700

    more packages

---
+++ cdms2/build.sh
@@ -0,0 +1,7 @@
+export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+cd Packages/cdms2
+python setup.py install
---
+++ libcf/build.sh
@@ -0,0 +1,8 @@
+export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+./configure --prefix=${PREFIX}
+make 
+make install
