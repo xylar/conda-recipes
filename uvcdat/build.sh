e396081ca586d0addb9caa585c6ce1c40ce6f961
Charles Doutriaux <doutriaux1@llnl.gov>
2016-03-18 08:21:58 -0700

    super hard build script for uvcdat

--- uvcdat/build.sh
+++ uvcdat/build.sh
@@ -1,7 +1,1 @@
-export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
-export CXXLAGS="${CFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
-
-cd Packages/cdms2
-python setup.py install
+echo "Nothing to do really, just setting up dependencies"
