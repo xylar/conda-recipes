526d414182c3b9e1768e2cb2cbcff1bc527b2e1b
Charles Doutriaux <doutriaux1@llnl.gov>
2016-03-18 10:42:09 -0700

    99% there

---
+++ vistrails/build.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
+export CXXLAGS="${CFLAGS}"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
+
+mkdir ${PREFIX}/vistrails
+cp -rf * ${PREFIX}/vistrails
+
+mkdir ${PREFIX}/bin
+sed "s#CONDAPREFIX#${PREFIX}#g;" scripts/uvcdat_for_conda.in > ${PREFIX}/bin/uvcdat
