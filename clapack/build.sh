b08988375777152d1a4749ace5ffb164c064728b
Denis Nadeau <dnadeau4@users.noreply.github.com>
2016-03-17 22:03:12 -0700

    fix clapack build

--- clapack/build.sh
+++ clapack/build.sh
@@ -3,5 +3,11 @@ export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
-make 
-make install
+cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX}
+make
+mkdir -p ${PREFIX}/lib
+mkdir -p ${PREFIX}/include
+cp  SRC/liblapack.a ${PREFIX}/lib
+cp  BLAS/SRC/libblas.a ${PREFIX}/lib
+cp  F2CLIBS/libf2c/libf2c.a ${PREFIX}/lib
+cp  -r INCLUDE/ ${PREFIX}/include/
