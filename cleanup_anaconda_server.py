67dd015226cc8b5d851b9418079bc763c41bc8f0
Charles Doutriaux <doutriaux1@llnl.gov>
2016-03-18 11:29:22 -0700

    uvcmetrics and server cleanup server

---
+++ cleanup_anaconda_server.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+import sys
+
+myos="osx-64"
+#myos = "linux-64"
+package = "*"
+channel = "uvcdat"
+buildstring = "UVCDAT"
+
+import glob
+pkg = glob.glob(package)
+print pkg
+for p in glob.glob(package):
+    print "PACKAGE:",p
+    try:
+        f= open(os.path.join(p,"meta.yaml"))
+        rd = f.read()
+        f.close()
+    except:
+        continue
+    rd = rd[rd.find("name:"):]
+    iname = rd.find("\n")
+    name = rd[:iname]
+    rd = rd[iname:]
+    name = name.split()[-1]
+    print "name:",name
+    rd = rd[rd.find("version:"):]
+    iversion = rd.find("\n")
+    version = rd[:iversion].split()[-1]
+    version="uvcdat_2.4.0"
+    print version
+    rd=rd[rd.find("string:"):]
+    ibuild = rd.find("\n")
+    build=rd[:ibuild].split()[-1]
+    print "build:",build
+    cmd = "anaconda remove -f %s/%s/%s/%s/%s-%s-%s.tar.bz2" % (channel,name,version,myos,name,version,build)
+    print cmd
+    os.system(cmd)
