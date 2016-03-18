1e87211b14165445e04d7f00cc0589bb8a8709d1
Denis Nadeau <dnadeau4@users.noreply.github.com>
2016-03-18 13:40:49 -0700

    fix fPIC flags

--- libcf/build.sh
+++ libcf/build.sh
@@ -2,6 +2,7 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
+export LFLAGS="-fPIC"
 
 ./configure --prefix=${PREFIX}
 #make  -j${CPU_COUNT}
