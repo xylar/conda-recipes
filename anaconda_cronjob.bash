fca437935244b7b7bbca69dd0be0a7607400e933
Charles Doutriaux <doutriaux1@llnl.gov>
2016-05-17 15:05:21 -0700

    added verbosity to cron job

--- anaconda_cronjob.bash
+++ anaconda_cronjob.bash
@@ -6,4 +6,4 @@ cd ${GIT_REPO}
 git pull
 cd ${SRC_DIR}
 export PATH=/export/doutriaux1/anaconda2/bin:$PATH
-python ./build_updated_packages.py -d 1 -u days -g ${GIT_REPO} -c uvcdat -l nighlty
+python ./build_updated_packages.py -d 1 -u days -g ${GIT_REPO} -c uvcdat -l nighlty -V
