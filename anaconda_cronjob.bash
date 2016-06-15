4a5e3a0b12b84b1976e14b59820667cd284628e2
Charles Doutriaux <doutriaux1@llnl.gov>
2016-06-15 09:18:37 -0700

    correct path

--- anaconda_cronjob.bash
+++ anaconda_cronjob.bash
@@ -5,5 +5,5 @@ GIT_REPO=${HOME}/anaconda-cron/uvcdat
 cd ${GIT_REPO}
 git pull
 cd ${SRC_DIR}
-export PATH=/export/doutriaux1/anaconda2/bin:$PATH
+export PATH=${HOME}/anaconda2/bin:$PATH
 python ./build_updated_packages.py -d 1 -u days -g ${GIT_REPO} -c uvcdat -l nightly -V
