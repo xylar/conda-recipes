59e29cf617b489a1fb9f226a06034ddd23734499
Charles Doutriaux <doutriaux1@llnl.gov>
2016-07-14 09:20:38 -0700

    switch to lower case

--- build_uvcdat_contrib.bash
+++ build_uvcdat_contrib.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-conda build EzTemplate -c doutriaux1 -c uvcdat
-conda build HDF5Tools -c doutriaux1 -c uvcdat
+conda build eztemplate -c doutriaux1 -c uvcdat
+conda build hdf5tools -c doutriaux1 -c uvcdat
 conda build asciidata -c doutriaux1 -c uvcdat
 conda build binaryio -c doutriaux1 -c uvcdat
 conda build cssgrid -c doutriaux1 -c uvcdat
@@ -12,4 +12,4 @@ conda build regridpack -c doutriaux1 -c uvcdat
 conda build shgrid -c doutriaux1 -c uvcdat
 conda build trends -c doutriaux1 -c uvcdat
 conda build spherepack -c doutriaux1 -c uvcdat
-conda build ZonalMeans -c doutriaux1 -c uvcdat
+conda build zonalmeans -c doutriaux1 -c uvcdat
