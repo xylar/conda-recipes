16e468334c15a2b0d75207d1c83d63aa137e0dab
Charles Doutriaux <doutriaux1@llnl.gov>
2016-10-31 13:12:02 -0700

    getting closer from np11 full conda-forge supported release

--- build_uvcdat_externals.bash
+++ build_uvcdat_externals.bash
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-#conda build hdf5
+conda build hdf5
 #conda build proj4
-#conda build libnetcdf
+conda build libnetcdf
 #conda build lapack
-#conda build clapack
-conda build ossuuid
-conda build libcf
-#conda build esmf
-#conda build jasper
-#conda build g2clib
-conda build yasm
-conda build x264
-conda build ffmpeg
-conda build cmor
-conda build vtk
-#conda build libdrs_c
-#conda build libcdms
-conda build libdrs_f
-conda build lats
-conda build ezget
-conda build cd77
+conda build clapack -c conda-forge
+conda build ossuuid -c conda-forge
+conda build gfortran -c conda-forge
+conda build libcf -c conda-forge
+conda build esmf --with-numpy ['1.9', '1.10', '1.11'] -c conda-forge
+conda build jasper -c conda-forge
+conda build g2clib -c conda-forge
+conda build yasm -c conda-forge
+conda build x264 -c conda-forge
+conda build ffmpeg -c conda-forge
+conda build cmor --with-numpy ['1.9', '1.10', '1.11'] -c conda-forge
+conda build vtk -c conda-forge
+conda build libdrs_c -c conda-forge
+conda build libcdms -c conda-forge
+conda build libdrs_f -c conda-forge
+conda build lats -c conda-forge
+conda build ezget -c conda-forge
+conda build cd77 -c conda-forge
