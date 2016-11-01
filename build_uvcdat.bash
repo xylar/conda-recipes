1c76d87e7c1da32eb42eb2c6d747bee8413cf37b
Charles Doutriaux <doutriaux1@llnl.gov>
2016-11-01 11:14:03 -0700

    linux builds all externals and packages

--- build_uvcdat.bash
+++ build_uvcdat.bash
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
-if [ $1"-" == "FULL-" ]; then ./build_uvcdat_externals.bash ; fi
-conda build cdat_info
-conda build distarray
-conda build cdms2
-conda build cdtime
-conda build cdutil
-conda build udunits2
-conda build unidata
-conda build genutil
-conda build dv3d
-conda build vcs
-conda build vcsaddons
-conda build thermo
-conda build wk
-conda build pydebug
-conda build uvcmetrics
-conda build vistrails
-conda build xmgrace
-conda build esg
+if [ $2"-" == "FULL-" ]; then ./build_uvcdat_externals.bash ; fi
+conda build -c conda-forge cdat_info
+conda build -c conda-forge distarray
+conda build -c conda-forge cdms2
+conda build -c conda-forge cdtime
+conda build -c conda-forge cdutil
+conda build -c conda-forge unidata
+conda build -c conda-forge genutil
+conda build -c conda-forge dv3d
+conda build -c conda-forge vcs
+conda build -c conda-forge vcsaddons
+conda build -c conda-forge thermo
+conda build -c conda-forge wk
+#conda build -c conda-forge pydebug
+conda build -c conda-forge uvcmetrics
+conda build -c conda-forge vistrails
+conda build -c conda-forge xmgrace
 if [ $1"-" == "FULL-" ]; then ./build_uvcdat_contrib.bash ; fi
