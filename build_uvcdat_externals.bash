4f679186db6ab14262d75cf9329c84e7cd835483
Charles Doutriaux <doutriaux1@llnl.gov>
2016-11-04 03:14:48 -0700

    good to go

--- build_uvcdat_externals.bash
+++ build_uvcdat_externals.bash
@@ -5,7 +5,6 @@
 #conda build lapack
 conda build clapack -c conda-forge
 conda build ossuuid -c conda-forge
-conda build gfortran -c conda-forge
 conda build libcf -c conda-forge
 conda build esmf -c conda-forge --numpy=1.11
 conda build esmf -c conda-forge --numpy=1.10
@@ -19,9 +18,9 @@ conda build vtk-cdat -c conda-forge
 conda build libdrs_c -c conda-forge
 conda build libcdms -c conda-forge
 conda build libdrs_f -c conda-forge
-conda build cmor -c conda-forge --numpy 1.11
-conda build cmor -c conda-forge --numpy 1.10
-conda build cmor -c conda-forge --numpy 1.9
+#conda build cmor -c conda-forge --numpy 1.11
+#conda build cmor -c conda-forge --numpy 1.10
+#conda build cmor -c conda-forge --numpy 1.9
 conda build lats -c conda-forge
 conda build ezget -c conda-forge
 conda build cd77 -c conda-forge
