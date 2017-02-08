855fba37ca9fe9854c1071ac13bf9ead47817e3b
Charles Doutriaux <doutriaux1@llnl.gov>
2017-02-07 18:02:29 -0800

    making it 32bit

--- mesalib/build.sh
+++ mesalib/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export CFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
-./configure  --prefix=$PREFIX --with-driver=osmesa --disable-gallium --disable-gallium-intel --disable-egl
-make
+export CFLAGS="-I${PREFIX}/include "${CFLAGS}
+export LDFLAGS="-L${PREFIX}/lib "${LDFLAGS}
+
+./configure  --prefix=$PREFIX --with-driver=osmesa --disable-gallium --disable-gallium-intel --disable-egl --with-osmesa-bits=32
+make -j${CPU_COUNT}
 make install
 
