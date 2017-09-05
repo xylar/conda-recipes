5c1b618b9afab17a13e298a3292bbfbba27ed178
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-05 08:20:17 -0700

    for 2.12 build

--- build_uvcdat_contrib.bash
+++ build_uvcdat_contrib.bash
@@ -1,28 +1,6 @@
 #!/usr/bin/env bash
-#conda build hdf5tools -c conda-forge
-#conda build asciidata -c conda-forge
-#conda build binaryio -c conda-forge --numpy=1.9
-#conda build cssgrid -c conda-forge --numpy=1.9
-#conda build dsgrid -c conda-forge --numpy=1.9
-conda build lmoments -c conda-forge --numpy=1.9
-conda build natgrid -c conda-forge --numpy=1.9
-conda build ort -c conda-forge --numpy=1.9
-conda build regridpack -c conda-forge --numpy=1.9
-conda build shgrid -c conda-forge --numpy=1.9
-conda build trends -c conda-forge --numpy=1.9
-conda build spherepack -c conda-forge --numpy=1.9
-conda build zonalmeans -c conda-forge --numpy=1.9
-conda build binaryio -c conda-forge --numpy=1.10
-conda build cssgrid -c conda-forge --numpy=1.10
-conda build dsgrid -c conda-forge --numpy=1.10
-conda build lmoments -c conda-forge --numpy=1.10
-conda build natgrid -c conda-forge --numpy=1.10
-conda build ort -c conda-forge --numpy=1.10
-conda build regridpack -c conda-forge --numpy=1.10
-conda build shgrid -c conda-forge --numpy=1.10
-conda build trends -c conda-forge --numpy=1.10
-conda build spherepack -c conda-forge --numpy=1.10
-conda build zonalmeans -c conda-forge --numpy=1.10
+conda build hdf5tools -c conda-forge
+conda build asciidata -c conda-forge
 conda build binaryio -c conda-forge --numpy=1.11
 conda build cssgrid -c conda-forge --numpy=1.11
 conda build dsgrid -c conda-forge --numpy=1.11
@@ -45,3 +23,14 @@ conda build shgrid -c conda-forge --numpy=1.12
 conda build trends -c conda-forge --numpy=1.12
 conda build spherepack -c conda-forge --numpy=1.12
 conda build zonalmeans -c conda-forge --numpy=1.12
+conda build binaryio -c conda-forge --numpy=1.13
+conda build cssgrid -c conda-forge --numpy=1.13
+conda build dsgrid -c conda-forge --numpy=1.13
+conda build lmoments -c conda-forge --numpy=1.13
+conda build natgrid -c conda-forge --numpy=1.13
+conda build ort -c conda-forge --numpy=1.13
+conda build regridpack -c conda-forge --numpy=1.13
+conda build shgrid -c conda-forge --numpy=1.13
+conda build trends -c conda-forge --numpy=1.13
+conda build spherepack -c conda-forge --numpy=1.13
+conda build zonalmeans -c conda-forge --numpy=1.13
