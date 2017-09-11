575b1a19c11d6a3096fe591f379f11c3747fa71a
Charles Doutriaux <doutriaux1@llnl.gov>
2017-09-11 08:51:57 -0700

    updated ;inning for acme_unified

--- acme_metapackage.bash
+++ acme_metapackage.bash
@@ -1,1 +1,51 @@
-conda metapackage acme-unified 1.0 --build-number 0 --dependencies cdat_info distarray cdms2 cdtime cdutil genutil vtk-cdat dv3d vcs vcsaddons thermo wk vistrails uvcmetrics xmgrace hdf5tools asciidata binaryio cssgrid dsgrid lmoments natgrid ort regridpack shgrid trends zonalmeans matplotlib basemap ipython jupyter nb_conda flake8 autopep8 spyder eofs windspharm cibots cdp output_viewer uvcdat scipy numpy netCDF4 xarray dask bottleneck lxml nco sympy pyproj pytest shapely cartopy
+export UVCDAT_VERSION=2.12
+export BUILD=0
+export VERSION=1.1
+conda metapackage acme-unified ${VERSION} --build-number ${BUILD}  \
+    --dependencies "cdat_info ==${UVCDAT_VERSION}" \
+"distarray ==${UVCDAT_VERSION}" \
+"cdms2 ==${UVCDAT_VERSION}" \
+"cdtime ==${UVCDAT_VERSION}" \
+"cdutil ==${UVCDAT_VERSION}" \
+"genutil ==${UVCDAT_VERSION}" \
+"vtk-cdat ==7.1.0.${UVCDAT_VERSION}" \
+"dv3d ==${UVCDAT_VERSION}" \
+"vcs ==${UVCDAT_VERSION}" \
+"vcsaddons ==${UVCDAT_VERSION}" \
+"thermo ==${UVCDAT_VERSION}" \
+"wk ==${UVCDAT_VERSION}" \
+"vistrails ==${UVCDAT_VERSION}" \
+"xmgrace ==${UVCDAT_VERSION}" \
+"hdf5tools ==${UVCDAT_VERSION}" \
+"asciidata ==${UVCDAT_VERSION}" \
+"binaryio ==${UVCDAT_VERSION}" \
+"cssgrid ==${UVCDAT_VERSION}" \
+"dsgrid ==${UVCDAT_VERSION}" \
+"lmoments ==${UVCDAT_VERSION}" \
+"natgrid ==${UVCDAT_VERSION}" \
+"ort ==${UVCDAT_VERSION}" \
+"regridpack ==${UVCDAT_VERSION}" \
+"shgrid ==${UVCDAT_VERSION}" \
+"trends ==${UVCDAT_VERSION}" \
+"zonalmeans ==${UVCDAT_VERSION}" \
+"cdp ==1.1.0" \
+"acme_diags ==v0.1b" \
+"cibots ==0.2" \
+"output_viewer ==1.2.2" \
+"xarray ==0.9.5" \
+"dask ==0.15.2" \
+"nco ==4.6.8" \
+"lxml ==3.8.0" \
+"sympy ==1.1.1" \
+"pyproj ==1.9.5.1" \
+"pytest ==3.2.2" \
+"shapely  ==1.6.1" \
+"cartopy  ==0.15.1" \
+"progressbar ==2.3" \
+"matplotlib" \
+"basemap" \
+"jupyter" \
+"nb_conda" \
+"ipython" \
+"bottleneck ==1.2.1" \
+"netcdf4 ==1.2.9"
