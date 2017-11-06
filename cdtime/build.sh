c7a877fb29d74fa8ffff08d4cec628500da1c704
Charles Doutriaux <doutriaux1@llnl.gov>
2017-11-06 11:10:35 -0800

    sometines no gfortran symlink

--- cdtime/build.sh
+++ cdtime/build.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env bash
 export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
+if [ $(uname) == "Linux" ];then
+    if [ ! -f ${PREFIX}/lib/libgfortran.so ]; then
+        ln -s ${PREFIX}/lib/libgfortran.so.3.0.0 ${PREFIX}/lib/libgfortran.so
+    fi
+fi
 python setup.py install
