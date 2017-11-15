c1ceab3269e0a34354556d2cad374d9b0b8efa45
zshaheen <zshaheen93@gmail.com>
2017-11-15 11:45:10 -0800

    Added dependencies requested by Xylar.

--- acme_metapackage.bash
+++ acme_metapackage.bash
@@ -19,15 +19,16 @@ conda metapackage -c conda-forge -c acme -c uvcdat -c opengeostat acme-unified $
 "acme_diags ${OPERATOR}1.0.1" \
 "cibots ${OPERATOR}0.2" \
 "xarray ${OPERATOR}0.9.6" \
-"dask ${OPERATOR}0.15.2" \
-"nco ${OPERATOR}4.6.9" \
-"lxml ${OPERATOR}3.8.0" \
+"dask ${OPERATOR}0.15.4" \
+"nco ${OPERATOR}4.7.0" \
+"lxml ${OPERATOR}4.1.1" \
 "sympy ${OPERATOR}1.1.1" \
 "pyproj ${OPERATOR}1.9.5.1" \
 "pytest ${OPERATOR}3.2.2" \
-"shapely  ${OPERATOR}1.6.1" \
+"shapely  ${OPERATOR}1.6.2" \
 "cartopy  ${OPERATOR}0.15.1" \
 "progressbar ${OPERATOR}2.3" \
+"pillow ${OPERATOR}4.3.0" \
 "scipy <1.0.0" \
 "matplotlib" \
 "basemap" \
@@ -37,5 +38,5 @@ conda metapackage -c conda-forge -c acme -c uvcdat -c opengeostat acme-unified $
 "nb_conda" \
 "ipython" \
 "bottleneck ${OPERATOR}1.2.1" \
-"netcdf4 ${OPERATOR}1.2.9" \
+"netcdf4 ${OPERATOR}1.3.1" \
 "pyevtk ${OPERATOR}1.0.0"
