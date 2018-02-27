434054b372ef1502f9e7bf1f55db924ba726f336
Charles Doutriaux <doutriaux1@llnl.gov>
2018-02-27 15:57:31 -0800

    adding vcdat to cdat, bash script to generate all 4 envs

---
+++ make_env.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+conda create -y -n cdat-3.0.beta_py2 -c uvcdat/label/v30beta -c anaconda -c conda-forge cdat
+conda create -y -n cdat-3.0.beta-nox_py2 -c uvcdat/label/v30beta -c anaconda -c conda-forge cdat mesalib
+conda create -y -n cdat-3.0.beta_py3 -c uvcdat/label/v30beta -c anaconda -c conda-forge -c nesii/label/dev-esmf cdat "python>3"
+conda create -y -n cdat-3.0.beta-nox_py3 -c uvcdat/label/v30beta -c anaconda -c conda-forge -c nesii/label/dev-esmf cdat mesalib "python>3"
+
+source activate cdat-3.0.beta_py2
+conda env export > cdat-3.0.beta_py2.$(uname).yaml 
+source activate cdat-3.0.beta-nox_py2
+conda env export > cdat-3.0.beta-nox_py2.$(uname).yaml 
+source activate cdat-3.0.beta_py3
+conda env export > cdat-3.0.beta_py3.$(uname).yaml 
+source activate cdat-3.0.beta-nox_py3
+conda env export > cdat-3.0.beta-nox_py3.$(uname).yaml 
