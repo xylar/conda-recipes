8cc30caefa48eb9ec0c0dd961e5904f77b67cb3b
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-16 10:54:21 -0700

    make env from nightlies

--- make_env.bash
+++ make_env.bash
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
-conda create -y -n cdat-3.0.beta_py2 -c uvcdat/label/v30beta -c anaconda -c conda-forge cdat "python<3"
-conda create -y -n cdat-3.0.beta-nox_py2 -c uvcdat/label/v30beta -c anaconda -c conda-forge cdat mesalib "python<3"
-conda create -y -n cdat-3.0.beta_py3 -c uvcdat/label/v30beta -c conda-forge -c nesii/label/dev-esmf cdat "python>3"
-conda create -y -n cdat-3.0.beta-nox_py3 -c uvcdat/label/v30beta -c conda-forge -c nesii/label/dev-esmf cdat mesalib "python>3"
+export RELEASE="3.0.beta2"
 
-source activate cdat-3.0.beta_py2
+conda create -y -n cdat-${RELEASE}_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat "python<3"
+source activate cdat-${RELEASE}_py2
 conda env export > cdat-3.0.beta_py2.$(uname).yaml 
-source activate cdat-3.0.beta-nox_py2
+
+source deactivate
+source activate base
+
+conda create -y -n cdat-${RELEASE}-nox_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat mesalib "python<3"
+source activate cdat-${RELEASE}-nox_py2
 conda env export > cdat-3.0.beta-nox_py2.$(uname).yaml 
-source activate cdat-3.0.beta_py3
-conda env export > cdat-3.0.beta_py3.$(uname).yaml 
+
+source deactivate
+source activate base
+
+conda create -y -n cdat-${RELEASE}_py3 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat "python>3"
+source activate cdat-${RELEASE}_py3
+conda env export > cdat-${RELEASE}_py3.$(uname).yaml 
+
+source deactivate
+source activate base
+
+conda create -y -n cdat-${RELEASE}-nox_py3 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat mesalib "python>3"
 source activate cdat-3.0.beta-nox_py3
-conda env export > cdat-3.0.beta-nox_py3.$(uname).yaml 
+conda env export > cdat-${RELEASE}-nox_py3.$(uname).yaml 
