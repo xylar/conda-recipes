12b8bb2040c8235652c427d4ceb77782906308c6
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-16 11:32:10 -0700

    correct yaml name

--- make_env.bash
+++ make_env.bash
@@ -4,14 +4,14 @@ export RELEASE="3.0.beta2"
 
 conda create -y -n cdat-${RELEASE}_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat "python<3"
 source activate cdat-${RELEASE}_py2
-conda env export > cdat-3.0.beta_py2.$(uname).yaml 
+conda env export > cdat-${RELEASE}_py2.$(uname).yaml 
 
 source deactivate
 source activate base
 
 conda create -y -n cdat-${RELEASE}-nox_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat mesalib "python<3"
 source activate cdat-${RELEASE}-nox_py2
-conda env export > cdat-3.0.beta-nox_py2.$(uname).yaml 
+conda env export > cdat-${RELEASE}-nox_py2.$(uname).yaml 
 
 source deactivate
 source activate base
