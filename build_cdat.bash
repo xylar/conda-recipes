d81760bfdae6725ca5dfc0c56b6afb85f2158639
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-22 17:33:19 -0700

    buuild update

--- build_cdat.bash
+++ build_cdat.bash
@@ -12,16 +12,16 @@ if [ $2"-" == "FULL-" ]; then ./build_uvcdat_externals.bash ; fi
 #conda build -c conda-forge genutil --numpy=1.10
 #conda build -c conda-forge genutil --numpy=1.9
 #conda build -c conda-forge cdutil
-conda build -c conda-forge dv3d --python=27
-conda build -c conda-forge dv3d --python=36
-conda build -c conda-forge vcs --python=27
-conda build -c conda-forge vcs --python=36
-conda build -c conda-forge vcsaddons --python=27
-conda build -c conda-forge vcsaddons --python=36
-conda build -c conda-forge thermo --python=27
-conda build -c conda-forge thermo --python=36
-conda build -c conda-forge wk --python=27
-conda build -c conda-forge wk --python=36
+conda build -c conda-forge dv3d --python=27 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge dv3d --python=36 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge vcs --python=27 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge vcs --python=36 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge vcsaddons --python=27 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge vcsaddons --python=36 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge thermo --python=27 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge thermo --python=36 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge wk --python=27 -c cdat/label/nightly -c conda-forge
+conda build -c conda-forge wk --python=36 -c cdat/label/nightly -c conda-forge
 #conda build -c conda-forge output_viewer
 #conda build -c conda-forge cdp
 #conda build -c conda-forge uvcmetrics --numpy=1.12
