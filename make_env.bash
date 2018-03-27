5d0d82d7da97a18e79ace98e2a3441be618b5e04
Charles Doutriaux <doutriaux1@llnl.gov>
2018-03-27 08:24:06 -0700

    no typo

--- make_env.bash
+++ make_env.bash
@@ -2,27 +2,27 @@
 
 export RELEASE="3.0.beta2"
 
-conda create -y -n cdat-${RELEASE}_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat "python<3"
-source activate cdat-${RELEASE}_py2
-conda env export > cdat-${RELEASE}_py2.$(uname).yaml 
+#conda create -y -n cdat-${RELEASE}_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat "python<3"
+#source activate cdat-${RELEASE}_py2
+#conda env export > cdat-${RELEASE}_py2.$(uname).yaml 
 
-source deactivate
-source activate base
+#source deactivate
+#source activate base
 
-conda create -y -n cdat-${RELEASE}-nox_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat mesalib "python<3"
-source activate cdat-${RELEASE}-nox_py2
-conda env export > cdat-${RELEASE}-nox_py2.$(uname).yaml 
+#conda create -y -n cdat-${RELEASE}-nox_py2 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat mesalib "python<3"
+#source activate cdat-${RELEASE}-nox_py2
+#conda env export > cdat-${RELEASE}-nox_py2.$(uname).yaml 
 
-source deactivate
-source activate base
+#source deactivate
+#source activate base
 
-conda create -y -n cdat-${RELEASE}_py3 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat "python>3"
-source activate cdat-${RELEASE}_py3
-conda env export > cdat-${RELEASE}_py3.$(uname).yaml 
+#conda create -y -n cdat-${RELEASE}_py3 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat "python>3"
+#source activate cdat-${RELEASE}_py3
+#conda env export > cdat-${RELEASE}_py3.$(uname).yaml 
 
-source deactivate
-source activate base
+#source deactivate
+#source activate base
 
 conda create -y -n cdat-${RELEASE}-nox_py3 -c cdat/label/nightly -c nesii/label/dev-esmf -c conda-forge -c uvcdat cdat mesalib "python>3"
-source activate cdat-3.0.beta-nox_py3
+source activate cdat-${RELEASE}-nox_py3
 conda env export > cdat-${RELEASE}-nox_py3.$(uname).yaml 
