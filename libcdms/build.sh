9e79e7501402bcf64d3f84489eac710f36a7fc6f
Charles Doutriaux <doutriaux1@llnl.gov>
2018-04-18 16:38:36 -0700

    changes necessary to make cdms2 compatible with netcdf 4.6

--- libcdms/build.sh
+++ libcdms/build.sh
@@ -1,7 +1,7 @@
-export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
-export CXXLAGS="${CFLAGS}"
-export CPPFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-L${PREFIX}/lib"
+#export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
+#export CXXLAGS="${CFLAGS}"
+#export CPPFLAGS="-I${PREFIX}/include"
+#export LDFLAGS="-L${PREFIX}/lib"
 
 ./configure \
     --enable-dap= \
