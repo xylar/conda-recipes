ddcc1d81115881245b968923da90889e8edfe7bf
Charles Doutriaux <doutriaux1@llnl.gov>
2018-04-18 13:34:51 -0700

    vtk-cdat

--- vtk-cdat/build.sh
+++ vtk-cdat/build.sh
@@ -17,10 +17,11 @@ else
         #export LD_LIBRARY_PATH=${PREFIX}/lib:${LIBRARY_PATH}
         #export CC=${CC}" -Wl,-rpath=${PREFIX}/lib"
         #export CXX=${CXX}" -Wl,-rpath=${PREFIX}/lib"
-        export CXXFLAGS="${CXXFLAGS} -I/usr/include"
-        export CPPFLAGS="${CPPFLAGS} -I/usr/include"
-        export CFLAGS="${CFLAGS} -I/usr/include"
-        export LDFLAGS="-L/usr/lib/x86_64-linux-gnu -lm"
+        export CXXFLAGS="-std=c++11 -I/usr/include ${CXXFLAGS} -I/usr/include"
+        export CPPFLAGS="-I/usr/include ${CPPFLAGS} -I/usr/include"
+        export CFLAGS="-I/usr/include ${CFLAGS} -I/usr/include"
+        #export LDFLAGS="-L/usr/lib/x86_64-linux-gnu -lm"
+        echo "BARE FLAGS: ${CFLAGS} ${CXXFLAGS}"
     fi
     MPI_ARGS=""
 fi
