396844e722f42551f24a484faa259a140c87039c
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-05-17 17:11:19 -0700

    add gcc_linux to cdtime

--- cdtime/build.sh
+++ cdtime/build.sh
@@ -3,6 +3,7 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
+export LDSHARED="$CC -shared -pthread" 
 
 if [ $(uname) == "Linux" ];then
     if [ ! -f ${PREFIX}/lib/libgfortran.so ]; then
