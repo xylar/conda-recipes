e39dfe32c21186d90272cfdbb9f3eb3fe60946d8
Denis Nadeau <dnadeau4@users.noreply.github.com>
2018-05-29 16:27:39 -0700

    move linker to Linux section

--- cdtime/build.sh
+++ cdtime/build.sh
@@ -3,9 +3,9 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
-export LDSHARED="$CC -shared -pthread" 
 
 if [ $(uname) == "Linux" ];then
+    export LDSHARED="$CC -shared -pthread" 
     if [ ! -f ${PREFIX}/lib/libgfortran.so ]; then
         ln -s ${PREFIX}/lib/libgfortran.so.3.0.0 ${PREFIX}/lib/libgfortran.so
     fi
