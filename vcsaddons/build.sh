c86cae41c08a3f3bc23fd2b5fe50dc0faa397772
Charles Doutriaux <doutriaux1@llnl.gov>
2018-06-22 17:00:24 -0700

    updates for vcsaddons

--- vcsaddons/build.sh
+++ vcsaddons/build.sh
@@ -2,5 +2,9 @@ export CFLAGS="-Wall -g -m64 -pipe -O2  -fPIC"
 export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
-
-python setup.py install
+if [[ `uname` == "Linux" ]]; then
+    export LDSHARED="-shared -pthread"
+else
+    export LDSHARED="-bundle -undefined dynamic_lookup"
+fi
+env LDSHARED="${CC} ${LDSHARED}" python setup.py install
