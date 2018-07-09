f4eedd8f07374417a403c8c8cd6cd2b5a77a28cd
Charles Doutriaux <doutriaux1@llnl.gov>
2018-07-09 11:35:08 -0700

    modif for genutil recipe new compilers

--- genutil/build.sh
+++ genutil/build.sh
@@ -3,4 +3,10 @@ export CXXLAGS="${CFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include"
 export LDFLAGS="-L${PREFIX}/lib"
 
-python setup.py install
+if [[ `uname` == "Linux" ]]; then
+    export LDSHARED_FLAGS="-shared -pthread"
+else
+    export LDSHARED_FLAGS="-bundle -undefined dynamic_lookup"
+fi
+export LDSHARED="$CC $LDSHARED_FLAGS"
+LDSHARED=$LDSHARED python setup.py install
