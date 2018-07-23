3aee7a965c14411f74ded877c0bad45eac2dafb1
Charles Doutriaux <doutriaux1@llnl.gov>
2018-07-23 16:19:02 -0700

    almost fully works on mac

--- vtk-cdat/build.sh
+++ vtk-cdat/build.sh
@@ -39,7 +39,7 @@ echo "PYLIB: "${PY_LIB}
 
 COMMON_ARGS="-DCMAKE_C_COMPILER=$CC \
         -DCMAKE_CXX_COMPILER=$CXX \
-        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_BUILD_TYPE=Debug \
         -DCMAKE_INSTALL_PREFIX=\"${PREFIX}\" \
         -DCMAKE_INSTALL_RPATH:STRING=\"${PREFIX}/lib\" \
         -DBUILD_DOCUMENTATION=OFF \
@@ -79,53 +79,41 @@ COMMON_ARGS="-DCMAKE_C_COMPILER=$CC \
         -DModule_vtkInteractionWidgets:BOOL=ON \
         -DModule_vtkIOCore:BOOL=ON \
         -DModule_vtkIOExport:BOOL=ON \
-        -DModule_vtkIOExportOpenGL:BOOL=ON \
         -DModule_vtkIOGeometry:BOOL=ON \
         -DModule_vtkIOImage:BOOL=ON \
         -DModule_vtkIOImport:BOOL=ON \
         -DModule_vtkRenderingCore:BOOL=ON \
         -DModule_vtkRenderingFreeType:BOOL=ON \
-        -DModule_vtkRenderingFreeTypeOpenGL:BOOL=ON \
         -DModule_vtkRenderingImage:BOOL=ON \
         -DModule_vtkRenderingLabel:BOOL=ON \
-        -DModule_vtkRenderingOpenGL:BOOL=ON \
         -DModule_vtkRenderingVolume:BOOL=ON \
-        -DModule_vtkRenderingVolumeOpenGL:BOOL=ON \
         -DModule_vtkViewsCore:BOOL=ON \
         -DModule_vtkViewsGeovis:BOOL=ON \
         -DModule_vtkIOFFMPEG:BOOL=ON \
         -DBUILD_SHARED_LIBS=ON \
-        -DModule_AutobahnPython:BOOL=ON \
         -DVTK_WRAP_PYTHON=ON \
         -DPYTHON_MAJOR_VERSION=${PYVER_SHORT} \
         -DPYTHON_EXECUTABLE=${PYTHON} \
         -DPYTHON_INCLUDE_PATH=${PREFIX}/include/python${PY_VER} \
         -DPYTHON_LIBRARY=${PREFIX}/lib/${PY_LIB} \
-        -DVTK_INSTALL_PYTHON_MODULE_DIR=${SP_DIR} \
         -DModule_vtkRenderingMatplotlib=ON \
         -DVTK_Group_Web:BOOL=ON \
-        -DLIBPROJ4_INCLUDE_DIR:PATH=${PREFIX}/include \
-        -DVTK_RENDERING_BACKEND=OpenGL \
+        -DVTK_RENDERING_BACKEND=OpenGL2 \
         -DVTK_USE_SYSTEM_ZLIB:BOOL=ON \
-        -DVTK_USE_SYSTEM_LIBXML2:BOOL=ON \
         -DVTK_USE_SYSTEM_HDF5:BOOL=ON \
-        -DVTK_USE_SYSTEM_NETCDF:BOOL=ON \
         -DVTK_USE_SYSTEM_FREETYPE:BOOL=ON \
-        -DVTK_USE_SYSTEM_LIBPROJ4:BOOL=ON \
-        -DVTK_Group_Rendering:BOOL=ON \
         -DVTK_Group_StandAlone:BOOL=OFF \
         -DVTK_LEGACY_SILENT:BOOL=ON\
-        ${SIX}"
+        "
 
 
 VTK_ARGS="\
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 \
-    -DVTK_REQUIRED_OBJCXX_FLAGS='' \
-    -DLIBPROJ4_LIBRARIES:FILEPATH=${PREFIX}/lib/libproj${SHLIB_EXT}"
+    -DVTK_REQUIRED_OBJCXX_FLAGS=''"
 
 if [[ ${CONDA_LST}'y' == *'mesalib'* ]]; then
     VTK_ARGS="${VTK_ARGS} \
-        -DVTK_USE_OFFSCREEN:BOOL=ON \
+        -DVTK_DEFAULT_RENDER_WINDOW_OFFSCREEN:BOOL=ON \
         -DVTK_OPENGL_HAS_OSMESA:BOOL=ON \
         -DOSMESA_INCLUDE_DIR:PATH=${PREFIX}/include \
         -DOSMESA_LIBRARY:FILEPATH=${PREFIX}/lib/libOSMesa32${SHLIB_EXT}"
@@ -140,7 +128,7 @@ if [[ ${CONDA_LST}'y' == *'mesalib'* ]]; then
     fi
 else
     VTK_ARGS="${VTK_ARGS} \
-        -DVTK_USE_OFFSCREEN:BOOL=OFF \
+        -DVTK_DEFAULT_RENDER_WINDOW_OFFSCREEN:BOOL=OFF \
         -DVTK_OPENGL_HAS_OSMESA:BOOL=OFF"
     if [ ${OSNAME} == Linux ]; then
         VTK_ARGS="${VTK_ARGS} \
@@ -159,4 +147,5 @@ echo $COMMAND
 eval ${COMMAND}
 
 make -j${CPU_COUNT}
+#make
 make install
