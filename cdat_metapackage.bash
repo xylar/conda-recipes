1f67581b782292ddd99f5edd4d051e24cb562068
Charles Doutriaux <doutriaux1@llnl.gov>
2018-08-22 15:01:00 -0700

    removed version dep for x264 in vtk-cdat, fixed typo in meta.yam of vcs-js, cdat metapkg for nightly

--- cdat_metapackage.bash
+++ cdat_metapackage.bash
@@ -1,7 +1,7 @@
 export VERSION=8.0
 export CDMS_VERSION=3.0
 export VERSION_EXTRA=""
-export BUILD=0
-export OPERATOR="="
+export BUILD=1
+export OPERATOR=">="
 export PY3_VERSION="<"
 conda metapackage cdat ${VERSION}${VERSION_EXTRA} --build-number ${BUILD} --dependencies "cdat_info ${OPERATOR}${VERSION}" "distarray ${OPERATOR}2.12.2" "cdms2 ${OPERATOR}${CDMS_VERSION}" "cdtime ${OPERATOR}${CDMS_VERSION}" "cdutil ${OPERATOR}${VERSION}" "genutil ${OPERATOR}${VERSION}" "vtk-cdat ${OPERATOR}8.0.1.${VERSION}" "dv3d ${OPERATOR}${VERSION}" "vcs ${OPERATOR}${VERSION}" "vcsaddons ${OPERATOR}${VERSION}" "thermo ${OPERATOR}${VERSION}" "wk ${OPERATOR}${VERSION}" matplotlib basemap ipython jupyter nb_conda flake8 autopep8 spyder eofs windspharm cibots cdp output_viewer esmpy scipy
