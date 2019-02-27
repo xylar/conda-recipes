88f120242f74b3788416a3b965f6249799a4953e
Charles Doutriaux <doutriaux1@llnl.gov>
2019-02-27 10:47:54 -0800

    simplier script

--- make_env.bash
+++ make_env.bash
@@ -7,27 +7,13 @@ export APPEND_CHANNEL=""
 
 source activate 
 
-conda create -y -n cdat-${RELEASE}_py27 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat "python<3"
-source activate cdat-${RELEASE}_py27
-conda env export > cdat-${RELEASE}_py27.$(uname).yaml 
+for PYVER in 2.7 3.6 3.7
+    do
+        echo "Doing version $PYVER"
+        conda create -y -n cdat-${RELEASE}_py$PYVER ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat python=$PYVER
+        source activate cdat-${RELEASE}_py$PYVER
+        conda env export > cdat-${RELEASE}_py$PYVER.$(uname).yaml 
+        source deactivate
+        source activate base
+    done
 
-source deactivate
-source activate base
-
-conda create -y -n cdat-${RELEASE}-nox_py27 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat mesalib "python<3"
-source activate cdat-${RELEASE}-nox_py27
-conda env export > cdat-${RELEASE}-nox_py27.$(uname).yaml 
-
-source deactivate
-source activate base
-
-conda create -y -n cdat-${RELEASE}_py36 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat "python=3.6"
-source activate cdat-${RELEASE}_py36
-conda env export > cdat-${RELEASE}_py36.$(uname).yaml 
-
-source deactivate
-source activate base
-
-conda create -y -n cdat-${RELEASE}-nox_py36 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat mesalib "python=3.6"
-source activate cdat-${RELEASE}-nox_py36
-conda env export > cdat-${RELEASE}-nox_py36.$(uname).yaml 
