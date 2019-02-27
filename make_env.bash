8b1105b9d4f7714e363aa1256d32d458c8366b3c
Charles Doutriaux <doutriaux1@llnl.gov>
2019-02-27 10:33:45 -0800

    cleaner make_env

--- make_env.bash
+++ make_env.bash
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
 
-export RELEASE="v80"
+export RELEASE="v81"
 export PREPEND_CHANNEL="-c cdat/label/test"
+export PREPEND_CHANNEL=""
 export APPEND_CHANNEL=""
 
 source activate 
 
-#conda create -y -n cdat-${RELEASE}_py2 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat "python<3"
-source activate cdat-${RELEASE}_py2
-conda env export > cdat-${RELEASE}_py2.$(uname).yaml 
+conda create -y -n cdat-${RELEASE}_py27 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat "python<3"
+source activate cdat-${RELEASE}_py27
+conda env export > cdat-${RELEASE}_py27.$(uname).yaml 
 
 source deactivate
 source activate base
 
-#conda create -y -n cdat-${RELEASE}-nox_py2 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat mesalib "python<3"
-source activate cdat-${RELEASE}-nox_py2
-conda env export > cdat-${RELEASE}-nox_py2.$(uname).yaml 
+conda create -y -n cdat-${RELEASE}-nox_py27 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat mesalib "python<3"
+source activate cdat-${RELEASE}-nox_py27
+conda env export > cdat-${RELEASE}-nox_py27.$(uname).yaml 
 
 source deactivate
 source activate base
 
-#conda create -y -n cdat-${RELEASE}_py3 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat "python>3"
-source activate cdat-${RELEASE}_py3
-conda env export > cdat-${RELEASE}_py3.$(uname).yaml 
+conda create -y -n cdat-${RELEASE}_py36 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat "python=3.6"
+source activate cdat-${RELEASE}_py36
+conda env export > cdat-${RELEASE}_py36.$(uname).yaml 
 
 source deactivate
 source activate base
 
-#conda create -y -n cdat-${RELEASE}-nox_py3 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat mesalib "python>3"
-source activate cdat-${RELEASE}-nox_py3
-conda env export > cdat-${RELEASE}-nox_py3.$(uname).yaml 
+conda create -y -n cdat-${RELEASE}-nox_py36 ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat mesalib "python=3.6"
+source activate cdat-${RELEASE}-nox_py36
+conda env export > cdat-${RELEASE}-nox_py36.$(uname).yaml 
