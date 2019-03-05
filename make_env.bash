cdd9994d4462d050466939fa6bb1690858339ad0
Charles Doutriaux <doutriaux1@llnl.gov>
2019-03-05 08:39:23 -0800

    set source or conda for activation command

--- make_env.bash
+++ make_env.bash
@@ -5,8 +5,10 @@ export PREPEND_CHANNEL="-c cdat/label/test"
 export PREPEND_CHANNEL=""
 export APPEND_CHANNEL=""
 
-conda deactivate
-conda activate base
+export ACTIVATE_COMMAND="source"
+
+$(${ACTIVATE_COMMAND} deactivate)
+$(${ACTIVATE_COMMAND} activate base)
 
 for PYVER in 2.7 3.6 3.7
     do
@@ -21,10 +23,10 @@ for PYVER in 2.7 3.6 3.7
             fi
             echo "Doing version $PYVER with mesalib set to $MESA"
             conda create -y -n cdat-${RELEASE}${MESA_NAME}_py$PYVER ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat python=$PYVER ${MESA_PKG}
-            conda activate cdat-${RELEASE}_py$PYVER
+            $(${ACTIVATE_COMMAND} activate cdat-${RELEASE}_py$PYVER)
             conda env export --no-builds > cdat-${RELEASE}${MEA_NAME}_py$PYVER.$(uname).yaml 
-            conda deactivate
-            conda activate base
+            $(${ACTIVATE_COMMAND} deactivate)
+            $(${ACTIVATE_COMMAND} activate base)
         done
     done
 
