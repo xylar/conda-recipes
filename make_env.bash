2c67b60295cc9b4e50e40373cf4c1b7d5e5835fe
Charles Doutriaux <doutriaux1@llnl.gov>
2019-03-05 08:05:18 -0800

    do not include build numbers

--- make_env.bash
+++ make_env.bash
@@ -12,7 +12,7 @@ for PYVER in 2.7 3.6 3.7
         echo "Doing version $PYVER"
         conda create -y -n cdat-${RELEASE}_py$PYVER ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat python=$PYVER
         source activate cdat-${RELEASE}_py$PYVER
-        conda env export > cdat-${RELEASE}_py$PYVER.$(uname).yaml 
+        conda env export --no-builds > cdat-${RELEASE}_py$PYVER.$(uname).yaml 
         source deactivate
         source activate base
     done
