6f82bcb6ad63e065d5b52b2fe489ea293ebb2fe7
Charles Doutriaux <doutriaux1@llnl.gov>
2019-03-05 08:16:43 -0800

    use conda rather than source

--- make_env.bash
+++ make_env.bash
@@ -5,15 +5,16 @@ export PREPEND_CHANNEL="-c cdat/label/test"
 export PREPEND_CHANNEL=""
 export APPEND_CHANNEL=""
 
-source activate 
+conda deactivate
+conda activate base
 
 for PYVER in 2.7 3.6 3.7
     do
         echo "Doing version $PYVER"
         conda create -y -n cdat-${RELEASE}_py$PYVER ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat python=$PYVER
-        source activate cdat-${RELEASE}_py$PYVER
+        conda activate cdat-${RELEASE}_py$PYVER
         conda env export --no-builds > cdat-${RELEASE}_py$PYVER.$(uname).yaml 
-        source deactivate
-        source activate base
+        conda deactivate
+        conda activate base
     done
 
