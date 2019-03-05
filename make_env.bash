53cae416db1aa6fa9f3ec616266d66f5f77a959a
Charles Doutriaux <doutriaux1@llnl.gov>
2019-03-05 08:53:59 -0800

    need to run with -i

--- make_env.bash
+++ make_env.bash
@@ -5,10 +5,13 @@ export PREPEND_CHANNEL="-c cdat/label/test"
 export PREPEND_CHANNEL=""
 export APPEND_CHANNEL=""
 
-export ACTIVATE_COMMAND="source"
+export ACTIVATE_COMMAND="conda"
+export CREATE="n"
 
-$(${ACTIVATE_COMMAND} deactivate)
-$(${ACTIVATE_COMMAND} activate base)
+echo "ACTIVATE COMMAND: ${ACTIVATE_COMMAND}"
+
+${ACTIVATE_COMMAND} deactivate
+${ACTIVATE_COMMAND} activate base
 
 for PYVER in 2.7 3.6 3.7
     do
@@ -22,11 +25,13 @@ for PYVER in 2.7 3.6 3.7
                 export MESA_PKG=""
             fi
             echo "Doing version $PYVER with mesalib set to $MESA"
-            conda create -y -n cdat-${RELEASE}${MESA_NAME}_py$PYVER ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat python=$PYVER ${MESA_PKG}
-            $(${ACTIVATE_COMMAND} activate cdat-${RELEASE}_py$PYVER)
+            if [ ${CREATE} == "y" ]; then
+                conda create -y -n cdat-${RELEASE}${MESA_NAME}_py$PYVER ${PREPEND_CHANNEL} -c cdat/label/${RELEASE} -c conda-forge ${APPEND_CHANNEL} cdat python=$PYVER ${MESA_PKG}
+            fi
+            ${ACTIVATE_COMMAND} activate cdat-${RELEASE}_py$PYVER
             conda env export --no-builds > cdat-${RELEASE}${MEA_NAME}_py$PYVER.$(uname).yaml 
-            $(${ACTIVATE_COMMAND} deactivate)
-            $(${ACTIVATE_COMMAND} activate base)
+            ${ACTIVATE_COMMAND} deactivate
+            ${ACTIVATE_COMMAND} activate base
         done
     done
 
