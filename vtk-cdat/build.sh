b16866d69235e91f837e9cddab60a7ace4ab5c22
Scott Wittenburg <scott.wittenburg@kitware.com>
2019-03-06 16:12:09 -0700

    Add a few more fixes supporting the new module system

--- vtk-cdat/build.sh
+++ vtk-cdat/build.sh
@@ -3,12 +3,22 @@
 mkdir build
 cd build
 
+# Use bash "Remove Largest Suffix Pattern" to get rid of all but major version number
+PYTHON_MAJOR_VERSION=${PY_VER%%.*}
+
+# These will help cmake find the right python
+PYTHON_H_FILE=$(find $PREFIX -name Python.h -type f)
+PYTHON_INCLUDE_DIR=$(dirname ${PYTHON_H_FILE})
+PYTHON_LIBRARY=$(find $PREFIX -type f -regex ".*libpython.*\.dylib$")
+PYTHON_INCLUDE_PARAMETER_NAME="Python${PYTHON_MAJOR_VERSION}_INCLUDE_DIR"
+PYTHON_LIBRARY_PARAMETER_NAME="Python${PYTHON_MAJOR_VERSION}_LIBRARY_RELEASE"
+
 BUILD_CONFIG=Release
 OSNAME=`uname`
 
 if [ -f "$PREFIX/lib/libOSMesa32${SHLIB_EXT}" ]; then
     VTK_ARGS="${VTK_ARGS} \
-        -DVTK_USE_OFFSCREEN:BOOL=ON \
+        -DVTK_DEFAULT_RENDER_WINDOW_OFFSCREEN:BOOL=ON \
         -DVTK_OPENGL_HAS_OSMESA:BOOL=ON \
         -DOSMESA_INCLUDE_DIR:PATH=${PREFIX}/include \
         -DOSMESA_LIBRARY:FILEPATH=${PREFIX}/lib/libOSMesa32${SHLIB_EXT}"
@@ -19,20 +29,18 @@ if [ -f "$PREFIX/lib/libOSMesa32${SHLIB_EXT}" ]; then
             -DVTK_USE_X:BOOL=OFF"
     elif [ ${OSNAME} == Darwin ]; then
         VTK_ARGS="${VTK_ARGS} \
-            -DVTK_USE_CARBON:BOOL=OFF \
             -DVTK_USE_COCOA:BOOL=OFF \
             -DCMAKE_OSX_SYSROOT:PATH=${CONDA_BUILD_SYSROOT}"
     fi
 else
     VTK_ARGS="${VTK_ARGS} \
-        -DVTK_USE_OFFSCREEN:BOOL=OFF \
+        -DVTK_DEFAULT_RENDER_WINDOW_OFFSCREEN:BOOL=OFF \
         -DVTK_OPENGL_HAS_OSMESA:BOOL=OFF"
     if [ ${OSNAME} == Linux ]; then
         VTK_ARGS="${VTK_ARGS} \
             -DVTK_USE_X:BOOL=ON"
     elif [ ${OSNAME} == Darwin ]; then
         VTK_ARGS="${VTK_ARGS} \
-            -DVTK_USE_CARBON:BOOL=OFF \
             -DVTK_USE_COCOA:BOOL=ON \
             -DCMAKE_OSX_SYSROOT:PATH=${CONDA_BUILD_SYSROOT}"
     fi
@@ -47,13 +55,16 @@ cmake .. -G "Ninja" \
     -DCMAKE_INSTALL_LIBDIR:PATH="lib" \
     -DCMAKE_INSTALL_RPATH:PATH="${PREFIX}/lib" \
     -DVTK_BUILD_DOCUMENTATION:BOOL=OFF \
-    -DVTK_BUILD_TESTING:STRING==OFF \
+    -DVTK_BUILD_TESTING:STRING=OFF \
     -DVTK_BUILD_EXAMPLES:BOOL=OFF \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DVTK_LEGACY_SILENT:BOOL=OFF \
     -DVTK_HAS_FEENABLEEXCEPT:BOOL=OFF \
     -DVTK_WRAP_PYTHON:BOOL=ON \
-    -DVTK_PYTHON_VERSION:STRING="${PY_VER}" \
+    -DVTK_PYTHON_VERSION:STRING="${PYTHON_MAJOR_VERSION}" \
+    -DPYTHON_EXECUTABLE:FILEPATH="${PREFIX}/bin/python" \
+    "-D${PYTHON_INCLUDE_PARAMETER_NAME}:PATH=${PYTHON_INCLUDE_DIR}" \
+    "-D${PYTHON_LIBRARY_PARAMETER_NAME}:FILEPATH=${PYTHON_LIBRARY}" \
     -DVTK_PYTHON_OPTIONAL_LINK:BOOL=ON \
     -DVTK_MODULE_ENABLE_VTK_PythonInterpreter:STRING=NO \
     -DVTK_MODULE_ENABLE_VTK_RenderingMatplotlib:STRING=YES \
